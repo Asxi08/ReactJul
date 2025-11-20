1a44b1f39d0959c926ead80c7bafc756e45b0fd8
Asxi08 <nafeelaashik@gmail.com>
2025-11-20 20:54:06 +0530

    Changed the app.jsx in app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <>
      <h1>Welcome to react</h1>
+     <h2>Ashik changed the code</h2>
      <h3>Thank you</h3>
      <p>Hello how are you!</p>
      <Home />
